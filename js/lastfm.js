1519c809d79db9ea5fc4e98938de183a7a2dbc4d
benjiae <benjaminang2009@gmail.com>
2024-02-27 20:03:50 -0300

    new cursor + now playing widget (thanks @laura.sofia on the nekoweb discord server)

---
+++ js/lastfm.js
@@ -0,0 +1,31 @@
+const BASE_URL = `https://lastfm-last-played.biancarosa.com.br/benjae_/latest-song`;
+
+const getTrack = async () => {
+    const request = await fetch(BASE_URL);
+    const json = await request.json();
+
+    let isPlaying = json.track['@attr']?.nowplaying || false;
+
+    if(!isPlaying) {
+        // HIDE PLAYER DIV
+    } else {
+        // SHOW PLAYER DIV
+    }
+
+    // SET IMAGE/TEXT HERE
+    // COVER IMAGE: json.track.image[1]['#text']
+
+    // TITLE: json.track.name
+    // ARTIST: json.track.artist['#text']
+
+    let items = `
+    <span style="color: #a6e3a1">${json.track.name}</span>
+    <br>
+    <span style="color: #a6e3a1">${json.track.artist['#text']}</span>
+    `
+
+    document.getElementById("listening").innerHTML = items
+};
+
+setInterval(() => { getTrack(); }, 10000);
+getTrack();
