2f1352d1d2cfba54d2bdd31052d763004d328099
benjiae <benjaminang2009@gmail.com>
2024-02-28 17:58:35 -0300

    yea

--- js/lastfm.js
+++ js/lastfm.js
@@ -1,25 +1,11 @@
 const BASE_URL = `https://lastfm-last-played.biancarosa.com.br/benjae_/latest-song`;
 const playing = document.getElementById("listening");
 
-playing.style.paddingBottom = "20%";
-
 const getTrack = async () => {
     const request = await fetch(BASE_URL);
     const json = await request.json();
 
     let isPlaying = json.track['@attr']?.nowplaying || false;
-
-    if(!isPlaying) {
-        // HIDE PLAYER DIV
-    } else {
-        // SHOW PLAYER DIV
-    }
-
-    // SET IMAGE/TEXT HERE
-    // COVER IMAGE: json.track.image[1]['#text']
-
-    // TITLE: json.track.name
-    // ARTIST: json.track.artist['#text']
 
     let items = `
     <img src="${json.track.image[1]['#text']}">
@@ -31,8 +17,12 @@ const getTrack = async () => {
     <span style="color: #a6e3a1">${json.track.artist['#text']}</span>
     `
 
-    playing.style.paddingBottom = "0px";
-    playing.innerHTML = items;
+    if(!isPlaying) {
+        playing.innerHTML = "Nothing.";
+    } else {
+        playing.innerHTML = items;
+    }
+    
 
 };
 setInterval(() => { getTrack(); }, 5000);
