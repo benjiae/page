953a642fe6e18aba95d787d594b6bbf624c7c124
benjae <benjaminang2009@gmail.com>
2024-09-22 11:44:39 -0300

    rewrite number i forgot

--- js/lastfm_box.js
+++
@@ -1,7 +0,0 @@
-lastfm_box = document.querySelector("#lastfm_box")
-
-lastfm_box.innerHTML = `
-                <h1 id="nowplaying" class="subtitle">I'm now listening to:</h1>
-                <div id="lastfm"><h1 class="subtitle">loading...</h1></div>
-                <p class="subtitle gray">thank you <a href="https://scrobbled.tepiloxtl.net/">tepiloxtl</a></p>
-                `
