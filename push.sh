1fead161b6fe1bc1283e51ba32c57972d3ff5625
benjae <benjae@sillydomain.name>
2025-11-23 13:41:33 -0300

    mrrow

--- push.sh
+++ push.sh
@@ -19,13 +19,11 @@ cd $build_directory
 git init
 git checkout -b $build_branch
 
-# ignore macOS system files
-echo ".DS_Store" > .gitignore
 # stage all files except .gitignore (don't want it in the static site)
 git add -- . ':!.gitignore'
 
 # commit static site files and force push to build_branch of the origin
-git commit -m "$*"
+git commit -m "push script: $*"
 git remote add origin ssh://git@codeberg.org/benjae/pages.git
 git remote set-url --add --push origin ssh://git@codeberg.org/benjae/pages.git
 git remote set-url --add --push origin git@github.com:benjiae/page.git
