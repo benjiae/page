ec33891368b154e13584fdc762de1412add34cee
benjae <benjae@sillydomain.name>
2026-06-26 16:06:02 -0400

    migrate to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://sillydomain.name',
-	integrations: [mdx({
-	   syntaxHighlight: 'shiki',
-	   shikiConfig: {
-	     theme: 'catppuccin-macchiato'
-	   }
-	 })]
+  site: "https://sillydomain.name",
+  integrations: [
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+        theme: "catppuccin-macchiato",
+      },
+    }),
+  ],
 });
--- push.sh
+++ push.sh
@@ -1,32 +1,4 @@
 #! /usr/bin/env bash
-# i stole this from https://codeberg.org/Codeberg/pages-server/issues/59
-build_directory="dist"
-build_branch="pages"
-message=$(git log -1 --format=%B)
 
-# delete previous site built, if it exists
-if [ -d "$build_directory" ]; then
-  echo "Found previous site build, deleting it"
-  rm -rf $build_directory
-fi
-
-echo "pushing this folder"
-git push
-
-echo "starting the other one"
-# generate static site
 npm run build
-
-# initialize a git repo in build_directory and checkout to build_branch
-cd $build_directory
-git init
-git checkout -b $build_branch
-
-# stage all files except .gitignore (don't want it in the static site)
-git add -- . ':!.gitignore'
-
-# commit static site files and force push to build_branch of the origin
-git commit -m "push script: $message"
-git remote add origin ssh://git@codeberg.org/benjae/pages.git
-git remote set-url --add --push origin ssh://git@codeberg.org/benjae/pages.git
-git push --force origin $build_branch
+rsync -ogvr --chown benja:benja dist/ vps:/var/www/sillydomain.name/
